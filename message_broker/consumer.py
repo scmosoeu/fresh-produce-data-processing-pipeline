bb7797546f52d0c2816aa52ed78ef83e93b0528d
scmosoeu <scmosoeu@gmail.com>
2024-06-01 23:04:33 +0200

    inserted comments in code

--- message_broker/consumer.py
+++ message_broker/consumer.py
@@ -3,7 +3,9 @@ import requests
 
 
 def on_message_received(ch, method, properties, body):
-
+    """
+    Subscribe a callback function to a queue
+    """
     commodity = body.decode('utf-8')
     response = requests.get(f"http://0.0.0.0:8000/{commodity}")
 
@@ -17,6 +19,7 @@ channel = connection.channel()
 
 channel.queue_declare(queue='letterbox')
 
+# Specify that the particular callback function should receive messages from the queue
 channel.basic_consume(queue='letterbox', auto_ack=True, on_message_callback=on_message_received)
 
 print("started consuming")
--- message_broker/producer.py
+++ message_broker/producer.py
@@ -1,16 +1,20 @@
 import pika
 
+# Connecting to a broker on our local machine, hence 'localhost'
 connection = pika.BlockingConnection(
     pika.ConnectionParameters('localhost')
 )
 
 channel = connection.channel()
 
+# creating a que to which messages will be delivered
 channel.queue_declare(queue='letterbox')
 
 # Everything needs to go through an exchange
 message = "apples"
 
+# default exchange is identified by an empty string
+# routing_key specifies exactly which queue the message should go
 channel.basic_publish(exchange='', routing_key='letterbox', body=message)
 
 print(f"sent message: {message}")
