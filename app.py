9a72083866221e0c1259ba84062917ed9e705f5a
scmosoeu <scmosoeu@gmail.com>
2024-07-15 20:41:12 +0200

    removed app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-from message_broker import consumer, producer
-
-receive_msg = producer.send_message('apples')
-print('Received msg:', receive_msg)
-
-consumed_msg = consumer.retrieve_message()
-print('Consumed msg:', consumed_msg)
